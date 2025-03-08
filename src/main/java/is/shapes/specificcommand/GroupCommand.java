405d8cfc730ec1e85736095887f50c39ba4ec2b6
toto <antoniolabate19@gmail.com>
2025-03-08 15:47:46 +0100

    now completed support for undo and redo operations on groups

--- src/main/java/is/shapes/specificcommand/GroupCommand.java
+++ src/main/java/is/shapes/specificcommand/GroupCommand.java
@@ -4,13 +4,16 @@ import is.command.Command;
 import is.shapes.model.AbstractGraphicObject;
 import is.shapes.model.GraphicObjectFactory;
 import is.shapes.model.GraphicObjectSingleton;
+import is.shapes.model.GroupObject;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class GroupCommand implements Command {
 
-    List<Long> ids;
+    private final List<Long> ids;
+    private GroupObject group;
+    private final List<AbstractGraphicObject> groupMembers = new ArrayList<>();
 
     public GroupCommand(List<Long> ids) {
         this.ids = new ArrayList<>(ids);
@@ -18,15 +21,24 @@ public class GroupCommand implements Command {
 
     @Override
     public boolean doIt() {
-        AbstractGraphicObject group = GraphicObjectFactory.createGroup(ids);
+        GraphicObjectSingleton singleton = GraphicObjectSingleton.getInstance();
+        for (Long id : ids) {
+            AbstractGraphicObject obj = singleton.getById(id);
+            if (obj != null) {
+                groupMembers.add(obj);
+            }
+        }
 
-        GraphicObjectSingleton.getInstance().add(group);
+        this.group = (GroupObject) GraphicObjectFactory.createGroup(ids);
+        singleton.add(group);
         System.out.printf("Created group with id %d\n", group.getId());
         return true;
     }
 
     @Override
     public boolean undoIt() {
+        GraphicObjectSingleton.getInstance().remove(group.getId());
+        System.out.printf("Ungrouped id %d\n", group.getId());
         return true;
     }
 }
--- src/main/java/is/shapes/specificcommand/UngroupCommand.java
+++ src/main/java/is/shapes/specificcommand/UngroupCommand.java
@@ -1,26 +1,38 @@
 package is.shapes.specificcommand;
 
 import is.command.Command;
+import is.shapes.model.AbstractGraphicObject;
 import is.shapes.model.GraphicObjectSingleton;
+import is.shapes.model.GroupObject;
+
+import java.util.*;
 
 public class UngroupCommand implements Command {
 
-    long id;
+    private final long id;
+    private final GroupObject originalGroup;
+    private final List<AbstractGraphicObject> groupMembers = new ArrayList<>();
 
     public UngroupCommand(long id) {
         this.id = id;
+        this.originalGroup = (GroupObject) GraphicObjectSingleton.getInstance().getById(id);
     }
 
     @Override
     public boolean doIt() {
-        GraphicObjectSingleton.getInstance().remove(id);
-        System.out.printf("Removed group with id %d\n", id);
+        for (AbstractGraphicObject obj : originalGroup) {
+            groupMembers.add(obj);
+        }
 
+        GraphicObjectSingleton.getInstance().remove(id);
+        System.out.printf("Ungrouped id %d\n", id);
         return true;
     }
 
     @Override
     public boolean undoIt() {
-        return false;
+        GraphicObjectSingleton.getInstance().add(originalGroup);
+        System.out.printf("Undid ungrouping of id %d\n", id);
+        return true;
     }
 }
